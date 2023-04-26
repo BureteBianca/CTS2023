d083ddae23612ee62816b6cc06c7af23e94edaee
BureteBianca <76430895+BureteBianca@users.noreply.github.com>
2023-04-26 21:08:54 +0300

    Add files via upload

---
+++ sem8/facade/main/Main.java
@@ -0,0 +1,13 @@
+package facade.main;
+
+import facade.clase.Facade;
+
+public class Main {
+    public static void main(String[] args) {
+        Facade facade = new Facade();
+        facade.interneazaPacient("Maria");
+        facade.interneazaPacient("Andrei");
+        facade.interneazaPacient("Frederico");
+        facade.interneazaPacient("Maria");
+    }
+}
